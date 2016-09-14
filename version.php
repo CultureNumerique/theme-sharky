760025b19dd7e574647fd6754365b351e1d1c308
freddylimpens <freddy.limpens@gmail.com>
2016-09-14 16:01:12 +0200

    --amend

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016090501;
+$plugin->version   = 2016091401;
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_sharky';
 $plugin->dependencies = array(
