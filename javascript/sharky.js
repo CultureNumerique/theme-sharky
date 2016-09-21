25b08021bb23e95c2bf1051454f9605a1e2641de
freddylimpens <freddy.limpens@gmail.com>
2016-09-21 17:07:12 +0200

    added proper allowfullscreen attribute on iframes

--- javascript/sharky.js
+++ javascript/sharky.js
@@ -3,7 +3,8 @@ console.log("changing iframe ??");
 iframes = document.getElementsByTagName('iframe');
 for (i = 0; i < iframes.length; i++) {
     ifr = iframes[i]
-    ifr.allowFullscreen = true;
+    // ifr.allowFullscreen = true;
+    ifr.setAttribute('allowfullscreen', '')
   }
 // is = i.src;
 // i.src = '';
